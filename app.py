e940f88ab34b40479927d11d19abcc193c741481
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2023-11-06 20:13:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def calculate():
+    if request.method == "POST":
+        principal = float(request.form["principal"])
+        rate = float(request.form["rate"]) / 100
+        time = float(request.form["time"])
+        compound_frequency = int(request.form["compound_frequency"])
+
+        amount = principal * (1 + rate/compound_frequency)**(compound_frequency*time)
+        result = amount - principal
+        return render_template("index.html", result=result)
+
+    return render_template("index.html", result=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
